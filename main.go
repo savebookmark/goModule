720cc19d1b7c78985eba3b727266f6593d8255e6
gogogo <gogowhatgo@aaa.cccaaaa>
2020-10-19 23:57:20 +0900

    workiva emirpasic

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/Workiva/go-datastructures/set"
+	"github.com/emirpasic/gods/lists/arraylist"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
@@ -22,6 +24,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	list := arraylist.New()
+	list.Add("list")
+
+	set := set.New()
+	set.Add("set")
+
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
