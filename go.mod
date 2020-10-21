1bea3c79d2dd2bad488a1cd13827333e44419640
gogogo <gogowhatgo@aaa.cccaaaa>
2020-10-21 13:45:41 +0900

    package test

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/savebookmark/testebiten
+
+go 1.15
+
+require (
+	github.com/Workiva/go-datastructures v1.0.52
+	github.com/emirpasic/gods v1.12.0
+	github.com/francoispqt/gojay v1.2.13
+	github.com/hajimehoshi/ebiten v1.12.1
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/tylertreat/BoomFilters v0.0.0-20200520150052-42a7b4300c0c
+)
--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/Workiva/go-datastructures/set"
-	"github.com/emirpasic/gods/lists/arraylist"
+	"github.com/francoispqt/gojay"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/labstack/echo/v4"
 )
+
+type user struct {
+	id    int
+	name  string
+	email string
+}
+
+// implement gojay.UnmarshalerJSONObject
+func (u *user) UnmarshalJSONObject(dec *gojay.Decoder, key string) error {
+	switch key {
+	case "id":
+		return dec.Int(&u.id)
+	case "name":
+		return dec.String(&u.name)
+	case "email":
+		return dec.String(&u.email)
+	}
+	return nil
+}
+func (u *user) NKeys() int {
+	return 3
+}
 
 // Game ..
 type Game struct{}
@@ -28,15 +52,61 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	list := arraylist.New()
-	list.Add("list")
+	// Echo instance
+	// e := echo.New()
 
-	set := set.New()
-	set.Add("set")
+	// Middleware
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
 
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	// Routes
+	// e.GET("/", hello)
+	// Start server
+	// e.Logger.Fatal(e.Start(":1323"))
+	/////////////////////////////////////////////
+	//gods
+	// list := arraylist.New()
+	// list.Add("list")
+	//workiva
+	// set := set.New()
+	// set.Add("set")
+	////////////////////////////////////////////////////
+	// ebiten.SetWindowSize(640, 480)
+	// ebiten.SetWindowTitle("Hello, World!")
+	// if err := ebiten.RunGame(&Game{}); err != nil {
+	// 	log.Fatal(err)
+	// }
+	///////////////////////////////////////////
+	// sbf := boom.NewDefaultStableBloomFilter(10000, 0.01)
+	// fmt.Println("stable point", sbf.StablePoint())
+
+	// sbf.Add([]byte(`a`))
+	// if sbf.Test([]byte(`a`)) {
+	// 	fmt.Println("contains a")
+	// }
+
+	// if !sbf.TestAndAdd([]byte(`b`)) {
+	// 	fmt.Println("doesn't contain b")
+	// }
+
+	// if sbf.Test([]byte(`b`)) {
+	// 	fmt.Println("now it contains b!")
+	// }
+
+	// Restore to initial state.
+	// sbf.Reset()
+	////////////////////////////////////////////////
+	u := &user{}
+	d := []byte(`{"id":1,"name":"gojay","email":"gojay@email.com"}`)
+	err := gojay.UnmarshalJSONObject(d, u)
+
+	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(u)
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
