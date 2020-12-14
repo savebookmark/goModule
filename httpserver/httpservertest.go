0242da852e483a3f4a0dafc7ba6947470d04055a
gogogo <gogowhatgo@aaa.cccaaaa>
2020-12-14 17:27:05 +0900

    ..

---
+++ httpserver/httpservertest.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<doctype html>
+			<html>
+				<head>
+					<title>Hello World</title>
+				</head>
+				<body>
+					Hello World!
+				</body>
+			</html>`,
+	)
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
